5dada761b6cbb7343646bc95486e2ada34e0a2ef
Alex Kashie <captainlalas22@gmail.com>
2024-02-23 11:47:32 -0500

    +aesthetic and geom attributes

--- data_viz_with_ggplot2.R
+++ data_viz_with_ggplot2.R
@@ -19,7 +19,8 @@ View(msleep)
 
 
 # Creation of the scatter plot in ggplot2 on penguins
-ggplot(data=penguins) + geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g))
+ggplot(data=penguins) + 
+    geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g))
 
 
 #We need to a bit of cleaning in our msleep data, grouping by vore with the average total sleep of each cat
@@ -31,4 +32,18 @@ View(msleep_group)
 # Plotting the msleep dataset with ggplot, with bar plot
 ggplot(data=msleep_group) + geom_bar(mapping = aes(x=vore))
 
+
+# Let's customize a bit the aesthetic of our penguins plot: color, shape, size, alpha
+ggplot(data=penguins) + 
+    geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g, color=species, shape=species))
+
+#And not the geom functions: point(scatter plot), jitter, smooth, line, bar and more
+ggplot(data=penguins) + 
+    geom_smooth(mapping = aes(x=flipper_length_mm, y=body_mass_g)) +
+    geom_jitter(mapping= aes(x=flipper_length_mm, y=body_mass_g))
+
+
+#Geom bar with the diamonds data which is already loaded in ggplot 2
+ggplot(data = diamonds) +
+    geom_bar(mapping = aes(x=cut, fill=cut))
                                       
